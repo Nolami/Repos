2d7d8d3f71268b94211204299899b32be92bbdc9
Nolami <94444366+Nolami@users.noreply.github.com>
2024-08-26 10:21:57 +0300

    Create the.cs

---
+++ the.cs
@@ -0,0 +1,68 @@
+using System;
+namespace The
+{
+    internal class Program
+    {
+        class Lib
+        {
+
+
+            private int due;
+            private int types;
+            private string name;
+
+            public int Due() { return due; }
+            public int Types() { return types; }
+            public string Name() { return name; }
+
+            public int NewDue ()
+            {
+                Console.WriteLine();
+                Console.Write("Give new due date amount: ");
+                due = int.Parse(Console.ReadLine());
+                return due;
+            }
+
+            public Lib()
+            {
+                due = 0;
+                types = 0;
+                name = "";
+
+            }
+            public Lib(int u_due, int u_types, string u_name)
+            {
+                due = u_due;
+                types = u_types;
+                name = u_name;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Let's try the library out!");
+            Console.WriteLine();
+            string u_name = "Fence";
+
+            Lib lib = new Lib(30, 1, u_name);
+
+            int d = lib.Due();
+            int ty = lib.Types();
+            string na = lib.Name();
+
+            Console.WriteLine($"Due date is: {d} days");
+            Console.WriteLine($"Type: {ty}");
+            Console.WriteLine($"Name: {na}");
+
+            d = lib.NewDue();
+            Console.WriteLine("");
+            Console.WriteLine("New values are:");
+            Console.WriteLine();
+
+            Console.WriteLine($"Due date is: {d} days");
+            Console.WriteLine($"Type: {ty}");
+            Console.WriteLine($"Name: {na}");
+
+        }
+    }
+}
